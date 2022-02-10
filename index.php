0d4f96d10dff6a7e358261eb4bf616550fc1a84b
Dario Bella <dbella@cerebotani.it>
2022-02-10 18:21:43 +0100

    ingraniamo

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Dario Bella">
+
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Il Ricettario di Dario</title>
+</head>
+<body>
+
+<div class="topbar">
+    <h2> Le tue ricette </h2>
+</div>
+
+<div class="recipeCard">
+    <img src="pics/gnocchi.jpg"> <h4>Gnocchi di patate</h4>
+</div>
+
+<div class="centerAddBtn">
+    <button id="addBtn" onclick="alert('aggiungiaml')"> <span class="material-icons">add_circle</span> </button>
+</div>
+</body>
+</html>
