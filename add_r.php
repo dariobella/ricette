496687831c201b8a9011d20527fb538cb9cdcf4a
Dario Bella <dbella@cerebotani.it>
2022-02-17 18:46:50 +0100

    keep adding

--- add_r.php
+++ add_r.php
@@ -2,7 +2,7 @@
 
 require_once "authorize.php";
 
-#var_export($_POST); die;
+var_export($_SESSION); die;
 
 $name = $_POST['name'] ?? '';
 $ingredients = $_POST['ingredients'] ?? '';
@@ -12,48 +12,39 @@ $type = $_POST['type'] ?? '';
 $hours = intval($_POST['hours']) ?? 0;
 $mins = intval($_POST['mins']) ?? 0;
 
-if ($year == '') $year = null;
-if ($price == '') $price = null;
 
-# ERO QUI
 if ($name == '') {
-    # --> restituire un messaggio di errore
     $_SESSION['add_data'] = [
-        'msg'  => 'Some required data is missing',
-        'title' => $title,
-        'genre_id' => $genre_id,
-        'year' => $year,
-        'price' => $price,
-        'authors_id' => $authors_id,
+        'name'  => $name,
+        'ingredients' => $ingredients,
+        'url' => $url,
+        'description' => $description,
+        'type' => $type,
+        'hours' => $hours,
+        'mins' => $mins,
     ];
-    header('location: /admin/books/add.php?');
+    header('location: add.php?');
     die;
 }
 
 try {
     $stmt = $db->prepare("
-        INSERT INTO books SET 
-            title = :title,
-            genre_id = :genre_id,
-            year = :year,
-            price = :price
+        SELECT id FROM users WHERE username=:username 
+    ");
+    $stmt->bindParam(':username', $user['username']);
+
+    $stmt = $db->prepare("
+        INSERT INTO recipes SET 
+            name = :name,
+            ingredients = :ingredients,
+            url = :url,
+            description = :description
         ");
     $stmt->bindParam(':title', $title);
     $stmt->bindParam(':genre_id', $genre_id);
     $stmt->bindParam(':year', $year);
     $stmt->bindParam(':price', $price);
     $stmt->execute();
-
-    $book_id = $db->lastInsertId();
-    $stmt = $db->prepare('
-        INSERT INTO books_authors VALUES ( :book_id, :author_id )
-    ');
-
-    $stmt->bindParam(':book_id', $book_id);
-    foreach ($authors_id as $author_id) {
-        $stmt->bindParam(':author_id', $author_id, PDO::PARAM_INT);
-        $stmt->execute();
-    }
 
 } catch (PDOException $e) {
     echo "Errore: " . $e->getMessage();
